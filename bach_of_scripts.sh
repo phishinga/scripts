c9695d7faa72a0bcdac8f734622cfe2ae0a0994b
phishinga.com <mfonic@phishinga.com>
2023-02-16 19:23:24 +0100

    update2

--- bach_of_scripts.sh
+++ bach_of_scripts.sh
@@ -1,1 +1,13 @@
+
+Use the kubectl get pods command to get a list of all Pods running in your cluster and the --output=json flag to output the Pod specifications in JSON format. 
+You can then use a tool like jq to filter the output and find the containers with runAsNonRoot set to true and a non-root runAsUser. 
+
+For example in bash:
+
 kubectl get pods --output=json | jq '.items[].spec.containers[] | select(.securityContext.runAsNonRoot == true and .securityContext.runAsUser != 0)'
+
+For example in PowerShell:
+
+kubectl get pods --output=json | ConvertFrom-Json | Select-Object -ExpandProperty items | Foreach-Object {
+    $_.spec.containers | Where-Object { $_.securityContext.runAsNonRoot -eq $true -and $_.securityContext.runAsUser -ne 0 }
+}
