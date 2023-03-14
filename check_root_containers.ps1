0f7d43b83b523bb8069283ff1766d53b6437a255
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-03-14 23:23:40 +0100

    Update check_root_containers.ps1

--- check_root_containers.ps1
+++ check_root_containers.ps1
@@ -1,38 +1,23 @@
-# Set the list of namespaces
-$namespaces = @("azure-vote", "big-monolith", "default", "kube-system", "secure-middleware")
+# Set the AKS cluster context
+kubectl config use-context <AKS_CLUSTER_NAME>
 
-# Loop through each namespace and pod to get the container name and user ID
-foreach ($ns in $namespaces) {
-    # Get the pods and containers in the namespace
-    $pods = kubectl get pods -n $ns -o jsonpath='{range .items[*]}{"NAMESPACE:"}{.metadata.namespace}{",PODNAME:"}{.metadata.name}{",CONTAINERNAME:"}{range .spec.containers[*]}{.name}{","}{end}{end}{"\n"}'
+# Get a list of all namespaces in the cluster
+$namespaces = kubectl get namespaces -o json | ConvertFrom-Json
+$namespaceNames = $namespaces.items.metadata.name
 
-    # Loop through each container in the pod and execute the "id" command
-    $pods | ForEach-Object {
-        # Split the pod info into separate variables
-        $namespace, $pod, $containers = $_ -split ",", 3
-        # Split the container names into an array
-        $containerNames = $containers -split ","
-
-        # Loop through each container and execute the "id" command
-        foreach ($container in $containerNames) {
-            Write-Host "Namespace: $namespace - Pod: $pod - Container: $container"
-            kubectl exec -n $namespace $pod -c $container -- sh -c "id"
+# Loop through all namespaces and check whether all containers are running as non-root users
+foreach ($ns in $namespaceNames) {
+    $pods = kubectl get pods -n $ns -o json | ConvertFrom-Json
+    foreach ($pod in $pods.items) {
+        foreach ($container in $pod.spec.containers) {
+            $securityContext = $container.securityContext
+            if ($securityContext) {
+                if ($securityContext.runAsNonRoot -ne $true) {
+                    Write-Host "Container '$($container.name)' in pod '$($pod.metadata.name)' in namespace '$ns' is running as root."
+                }
+            } else {
+                Write-Host "No security context set for container '$($container.name)' in pod '$($pod.metadata.name)' in namespace '$ns'."
+            }
         }
     }
-}
-
-
-
-# Read the list from the file
-$list = Get-Content -Path "path/to/list.txt"
-
-# Loop through each line in the list
-foreach ($line in $list) {
-    # Split the line into separate variables
-    $namespace = ($line -split ",")[0].Split(":")[1]
-    $pod = ($line -split ",")[1].Split(":")[1]
-    $container = ($line -split ",")[2].Split(":")[1]
-
-    # Execute the command
-    kubectl exec -n $namespace $pod -c $container -- sh -c "id"
 }
