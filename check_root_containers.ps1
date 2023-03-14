a2ab579ecb5a2a90e8a75c28bcaa64123163888c
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-03-14 11:12:09 +0100

    Update check_root_containers.ps1

--- check_root_containers.ps1
+++ check_root_containers.ps1
@@ -1,19 +1,16 @@
-# Set the name of your AKS cluster
-$clusterName = "<your-cluster-name>"
+#!/bin/bash
 
-# Get a list of all nodes in the cluster
-$nodes = kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+# Get the list of pods and containers
+PODS=$(kubectl get pods --all-namespaces -o jsonpath='{range .items[*]}{"NAMESPACE:"}{.metadata.namespace}{",PODNAME:"}{.metadata.name}{",CONTAINERNAMES:"}{range .spec.containers[*]}{.name}{","}{end}{"\n"}{end}')
 
-# Loop through each node and check for running containers
-foreach ($node in $nodes) {
-    Write-Host "Checking containers on node $node..."
+# Loop through each pod and container and execute the "id" command
+while read -r pod; do
+  ns=$(echo "$pod" | cut -d',' -f1 | cut -d':' -f2)
+  pod_name=$(echo "$pod" | cut -d',' -f2 | cut -d':' -f2)
+  containers=$(echo "$pod" | cut -d',' -f3 | cut -d':' -f2-)
 
-    # Get a list of running containers on the node
-    $containers = ssh $node docker ps -q
-
-    # Loop through each container and check if it's running as root
-    foreach ($container in $containers) {
-        Write-Host "Checking container $container on node $node..."
-        ssh $node docker exec -it $container sh -c "if [ \$(id -u) -eq 0 ]; then echo 'Container is running as root'; else echo 'Container is not running as root'; fi"
-    }
-}
+  for container in $(echo "$containers" | tr ',' ' '); do
+    echo "Namespace: $ns - Pod: $pod_name - Container: $container"
+    kubectl exec -n "$ns" "$pod_name" -c "$container" -- sh -c "id"
+  done
+done <<< "$PODS"
