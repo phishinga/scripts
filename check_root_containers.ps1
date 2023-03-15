5b9925fbd05bc7dc05871e3a7140ca004ecf9d61
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-03-15 10:28:22 +0100

    Update check_root_containers.ps1

--- check_root_containers.ps1
+++ check_root_containers.ps1
@@ -21,3 +21,33 @@ foreach ($ns in $namespaceNames) {
         }
     }
 }
+
+
+
+=========
+
+# Set the AKS cluster context
+kubectl config use-context <AKS_CLUSTER_NAME> | Write-Host
+
+# Get a list of all namespaces in the cluster
+$namespaces = kubectl get namespaces -o json | ConvertFrom-Json
+$namespaceNames = $namespaces.items.metadata.name
+
+# Loop through all namespaces and check whether all containers are running as non-root users
+foreach ($ns in $namespaceNames) {
+    Write-Host "Checking namespace: $ns"
+    $pods = kubectl get pods -n $ns -o json | ConvertFrom-Json
+    foreach ($pod in $pods.items) {
+        foreach ($container in $pod.spec.containers) {
+            $securityContext = $container.securityContext
+            if ($securityContext) {
+                if ($securityContext.runAsNonRoot -ne $true) {
+                    Write-Host "Container '$($container.name)' in pod '$($pod.metadata.name)' in namespace '$ns' is running as root."
+                    kubectl describe pod $($pod.metadata.name) -n $ns | Write-Host
+                }
+            } else {
+                Write-Host "No security context set for container '$($container.name)' in pod '$($pod.metadata.name)' in namespace '$ns'."
+            }
+        }
+    }
+}
