b3ec206e5bf596f673e500c78ad028aea2685769
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-03-22 14:36:01 +0100

    Create check_root_containers3.ps1

---
+++ check_root_containers3.ps1
@@ -0,0 +1,16 @@
+# Get JSON output for all the pods
+$json_output = kubectl get pods --all-namespaces -o json | ConvertFrom-Json
+
+# Iterate through each pod and container, check if the container is running as root using the security context
+foreach ($item in $json_output.items) {
+    $ns = $item.metadata.namespace
+    $pod_name = $item.metadata.name
+    
+    foreach ($container in $item.spec.containers) {
+        $container_name = $container.name
+        $runAsUser = $container.securityContext.runAsUser
+        $runningAsRoot = $null -eq $runAsUser -or $runAsUser -eq 0
+        
+        Write-Host "Namespace: $ns - Pod: $pod_name - Container: $container_name - Running as root: $runningAsRoot"
+    }
+}
