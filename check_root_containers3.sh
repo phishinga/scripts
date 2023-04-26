ae7174af44ddb36d407cdec6624c16645c80a18d
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-04-26 09:20:33 +0200

    Update check_root_containers3.sh

--- check_root_containers3.sh
+++ check_root_containers3.sh
@@ -15,6 +15,7 @@ echo "Motivation:
 Running Kubernetes containers as root can lead to security vulnerabilities and malicious attacks, as well as unintended changes to the host system. It's best to avoid running containers as root and use a non-root user with minimal privileges instead.
 
 This script retrieves a JSON output of all pods in a Kubernetes cluster and checks if any of the containers are running as root by examining their security context. It then outputs the namespace, pod name, container name, and whether it's running as root or not. 
+"
 
 # Get JSON output for all the pods
 json_output=$(kubectl get pods --all-namespaces -o json)
