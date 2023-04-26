0d32e4f0e28675fb2e76dda82ac15bd34bc465f7
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-04-26 09:48:53 +0200

    Update check_root_containers5.sh

--- check_root_containers5.sh
+++ check_root_containers5.sh
@@ -2,23 +2,24 @@
 
 # Print script header
 cat << EOF
-  ###############   PHISHINGA.COM  ###################
-  #                                                  #
-  #  Script that checks if K8S containers            #
-  #  are running under root                          #
-  #  Uses kubectl                                    #
-  #                                                  #
-  ####################################################
-  
-  Motivation:
-  Running Kubernetes containers as root can lead to security vulnerabilities and malicious attacks, 
-  as well as unintended changes to the host system. It's best to avoid running containers 
-  as root and use a non-root user with minimal privileges instead.
 
-  What does it do:
-  This script retrieves a JSON output of all pods in a Kubernetes cluster and checks 
-  if any of the containers are running as root by examining their security context. 
-  It then outputs the namespace, pod name, container name, and whether it's running as root or not.
+###############   PHISHINGA.COM  ###################
+#                                                  #
+#  Script that checks if K8S containers            #
+#  are running under root                          #
+#  Uses kubectl                                    #
+#                                                  #
+####################################################
+  
+Motivation:
+Running Kubernetes containers as root can lead to security vulnerabilities and malicious attacks, 
+as well as unintended changes to the host system. It's best to avoid running containers 
+as root and use a non-root user with minimal privileges instead.
+
+What does it do:
+This script retrieves a JSON output of all pods in a Kubernetes cluster and checks 
+if any of the containers are running as root by examining their security context. 
+It then outputs the namespace, pod name, container name, and whether it's running as root or not.
   
 EOF
 
