ece7e7e7ab815e26670d80d4dddf659a972259e5
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-04-26 16:27:07 +0200

    Update check_url_status.bat

--- check_url_status.bat
+++ check_url_status.bat
@@ -1,20 +1,18 @@
 @echo off
-echo.
-echo "############### PHISHINGA.COM ###################"
 
 for /L %%i in (1,1,20) do (
-set "response="
-for /f "delims=" %%A in ('curl --write-out %%{http_code}%% --silent --output nul http://emphonic.com') do set "response=%%A"
+  set "response="
+  for /f "delims=" %%A in ('curl --write-out %%{http_code}%% --silent --output nul http://emphonic.com') do set "response=%%A"
 
-if %response% equ 200 (
-echo %time% : Iteration %%i: The website is up and running. Response code: %response%
-) elseif %response% equ 404 (
-echo %time% : Iteration %%i: The page was not found. Response code: %response%
-) elseif %response% equ 429 (
-echo %time% : Iteration %%i: Rate limit is exceeded. Response code: %response%
-) else (
-echo %time% : Iteration %%i: An unexpected response was received. Response code: %response%
-)
+  if "%response%" equ "200" (
+    echo %time% : Iteration %%i: The website is up and running. Response code: %response%
+  ) elseif "%response%" equ "404" (
+    echo %time% : Iteration %%i: The page was not found. Response code: %response%
+  ) elseif "%response%" equ "429" (
+    echo %time% : Iteration %%i: Rate limit is exceeded. Response code: %response%
+  ) else (
+    echo %time% : Iteration %%i: An unexpected response was received. Response code: %response%
+  )
 
-timeout /T 1 >nul
+  timeout /T 1 >nul
 )
