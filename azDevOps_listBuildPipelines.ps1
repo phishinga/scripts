c94e64fcaf88d55a0eed5b15b250daee77a7664d
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-06-08 14:54:34 +0200

    Update azDevOps_listBuildPipelines.ps1

--- azDevOps_listBuildPipelines.ps1
+++ azDevOps_listBuildPipelines.ps1
@@ -1,22 +1,28 @@
-# Set your personal access token, organization, and project
+# Set your personal access token and organization
 $personalAccessToken = "YOUR_PERSONAL_ACCESS_TOKEN"
 $organization = "YOUR_ORGANIZATION"
-$project = "YOUR_PROJECT"
 
 # Create the authorization header
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($personalAccessToken)"))
 $headers = @{Authorization=("Basic {0}" -f $base64AuthInfo)}
 
-# Get a list of build pipelines
-$pipelinesUrl = "https://dev.azure.com/$organization/$project/_apis/build/definitions?api-version=6.0"
-$pipelinesResponse = Invoke-RestMethod -Uri $pipelinesUrl -Headers $headers -Method Get
-$pipelinesResponse.value | ForEach-Object {
-    # Get a list of runs for each pipeline
-    $runsUrl = "https://dev.azure.com/$organization/$project/_apis/build/builds?definitions=$($_.id)&api-version=6.0"
-    $runsResponse = Invoke-RestMethod -Uri $runsUrl -Headers $headers -Method Get
+# Get a list of all projects
+$projectsUrl = "https://dev.azure.com/$organization/_apis/projects?api-version=6.0"
+$projectsResponse = Invoke-RestMethod -Uri $projectsUrl -Headers $headers -Method Get
+$projectsResponse.value | ForEach-Object {
+    $project = $_.name
 
-    # Get the most recent run
-    $lastRun = $runsResponse.value | Sort-Object startTime -Descending | Select-Object -First 1
+    # Get a list of build pipelines for each project
+    $pipelinesUrl = "https://dev.azure.com/$organization/$project/_apis/build/definitions?api-version=6.0"
+    $pipelinesResponse = Invoke-RestMethod -Uri $pipelinesUrl -Headers $headers -Method Get
+    $pipelinesResponse.value | ForEach-Object {
+        # Get a list of runs for each pipeline
+        $runsUrl = "https://dev.azure.com/$organization/$project/_apis/build/builds?definitions=$($_.id)&api-version=6.0"
+        $runsResponse = Invoke-RestMethod -Uri $runsUrl -Headers $headers -Method Get
 
-    Write-Output ("Pipeline ID: {0}, Name: {1}, Created On: {2}, Revision: {3}, Type: {4}, Last Run: {5}, Last Run By: {6}" -f $_.id, $_.name, $_.createdDate, $_.revision, $_.type, $lastRun.startTime, $lastRun.requestedBy.displayName)
+        # Get the most recent run
+        $lastRun = $runsResponse.value | Sort-Object startTime -Descending | Select-Object -First 1
+
+        Write-Output ("Project: {0}, Pipeline ID: {1}, Name: {2}, Created On: {3}, Revision: {4}, Type: {5}, Last Run: {6}, Last Run By: {7}" -f $project, $_.id, $_.name, $_.createdDate, $_.revision, $_.type, $lastRun.startTime, $lastRun.requestedBy.displayName)
+    }
 }
