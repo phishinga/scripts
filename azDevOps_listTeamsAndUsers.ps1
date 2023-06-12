fee644dcd665d33ff15b3ee2d83f78deabf691fe
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-06-12 06:15:14 -0600

    Update azDevOps_listTeamsAndUsers.ps1

--- azDevOps_listTeamsAndUsers.ps1
+++ azDevOps_listTeamsAndUsers.ps1
@@ -1,6 +1,6 @@
 # Set your personal access token and organization
-$personalAccessToken = "YOUR_PERSONAL_ACCESS_TOKEN"
-$organization = "YOUR_ORGANIZATION"
+$personalAccessToken = "XXX-XXX-XXX-XXX"
+$organization = "XXX-XXX-XXX-XXX"
 
 # Create the authorization header
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($personalAccessToken)"))
@@ -14,7 +14,7 @@ $projectsResponse.value | ForEach-Object {
     $project = $_.name
 
     # Get a list of teams for each project
-    $teamsUrl = "https://dev.azure.com/$organization/$project/_apis/teams?api-version=6.0"
+    $teamsUrl = "https://dev.azure.com/$organization/_apis/teams?api-version=6.0"
     $teamsResponse = Invoke-RestMethod -Uri $teamsUrl -Headers $headers -Method Get
 
     $teamsResponse.value | ForEach-Object {
@@ -25,7 +25,7 @@ $projectsResponse.value | ForEach-Object {
         $membersResponse = Invoke-RestMethod -Uri $membersUrl -Headers $headers -Method Get
 
         $membersResponse.value | ForEach-Object {
-            Write-Output ("Project: {0}, Team: {1}, Member: {2}" -f $project, $team, $_.displayName)
+            Write-Output ("Project: {0}, Team: {1}, Member: {2}" -f $project, $team, $_.identity.displayName)
         }
     }
 }
