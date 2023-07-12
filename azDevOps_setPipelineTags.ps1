7be15680ec54e62965c854828806a6057a3586ea
phishinga.com <121772502+phishinga@users.noreply.github.com>
2023-07-12 11:25:28 -0600

    Update azDevOps_setPipelineTags.ps1

--- azDevOps_setPipelineTags.ps1
+++ azDevOps_setPipelineTags.ps1
@@ -1,34 +1,25 @@
-# Get a list of all projects
+# Set repo tags
 
 # Set your personal access token and organization
 $personalAccessToken = "XXX-XXX-XXX-XXX"
 $organization = "XXX-XXX-XXX-XXX"
 $project = "XXX-XXX-XXX-XXX"
-$definitionID = "XXX-XXX-XXX-XXX"
-
+$definitionId = "XXX-XXX-XXX-XXX"
 
 # Create the authorization header
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($personalAccessToken)"))
-$headers = @{Authorization=("Basic {0}" -f $base64AuthInfo)}
-
-# Lets call someone
-#$projectsUrl = "https://dev.azure.com/$organization/_apis/projects?api-version=6.0"
-$projectsUrl = "https://dev.azure.com/$organization/$project/_apis/build/definitions/$definitionID/tags?api-version=6.0-preview.2"
-$projectsResponse = Invoke-RestMethod -Uri $projectsUrl -Headers $headers -Method Post -Body (ConvertTo-Json $requestBody)
-#$projectsResponse.value | ForEach-Object { Write-Output ("Project: {0}" -f $_.name) }
-
-# Insert tag values
-$requestBody = @{
-    tags = @("TAG1")
+$headers = @{
+    #Authorization=("Basic {0}" -f $base64AuthInfo)
+    Authorization = "Basic $base64AuthInfo"
+    "Content-Type" = "application/json"
 }
 
+# Set URL
+$projectsUrl = "https://dev.azure.com/$organization/$project/_apis/build/definitions/$definitionId/tags/AppSecEngr?api-version=6.0-preview.2"
+
+# Lets call someone
+$projectsResponse = Invoke-RestMethod -Uri $projectsUrl -Headers $headers -Method Put 
+
 # Print it out baby
+#$projectsResponse.value | ForEach-Object { Write-Output ("Project: {0}" -f $_.name) }
 $projectsResponse
-
-
-
-
-Invoke-RestMethod : {"$id":"1","innerException":null,"message":"The request indicated a Content-Type of \"application/x-www-form-urlencoded\" for 
-method type \"POST\" which is not supported. Valid content types for this method are: application/json, 
-application/json-patch+json.","typeName":"Microsoft.VisualStudio.Services.WebApi.VssRequestContentTypeNotSupportedException, 
-Microsoft.VisualStudio.Services.WebApi","typeKey":"VssRequestContentTypeNotSupportedException","errorCode":0,"eventId":3000}
